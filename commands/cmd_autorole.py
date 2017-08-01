eff608bd1f471c7358f28defd7246936d4cc62a3
zekroTJA <zekrommaster110@aol.de>
2017-08-01 16:11:00 +0200

    updated stuff

--- commands/cmd_autorole.py
+++ commands/cmd_autorole.py
@@ -2,7 +2,10 @@ import os
 from os import path
 
 import discord
-from discord import Embed
+
+
+def error(content, channel, client):
+    yield from client.send_message(channel, embed=discord.Embed(color=discord.Color.red(), description=content))
 
 
 def get(server):
@@ -14,22 +17,27 @@ def get(server):
         return None
 
 
-def safeFile(id, server):
+def saveFile(id, server):
     if not path.isdir("SETTINGS/" + server.id):
         os.makedirs("SETTINGS/" + server.id)
-    with open("SETTINGS/" + server.id + "/autorole", "w") as fw:
-        fw.write(id)
-        fw.close()
+    with open("SETTINGS/" + server.id + "/autorole", "w") as f:
+        f.write(id)
+        f.close()
 
 
-def ex(message, args, client):
+def ex(args, message, client, invoke):
 
-    if len(args) > 1:
-        if args[0] == "add":
-            rolename = args[1:].__str__()[1:-1].replace("'", "").replace(",", "")
+    print(args)
+
+    if len(args) > 0:
+        rolename = args.__str__()[1:-1].replace(",", "").replace("'", "")
+        role = discord.utils.get(message.server.roles, name=rolename)
+        if role == None:
+            yield from error("Please enter a valid role existing on this server!", message.channel, client)
+        else:
             try:
-                role = discord.utils.get(message.server.roles, name=rolename)
-                safeFile(role.id, message.server)
-                yield from client.send_message(message.channel, embed=Embed(color=discord.Color.green(), description=("Successfully set autorole to role `%s`." % role.name)))
-            except:
-                yield from client.send_message(message.channel, embed=Embed(color=discord.Color.red(), description=("Role `%s` does not exist on this server!" % rolename)))
+                saveFile(role.id, message.server)
+                yield from client.send_message(message.channel, embed=discord.Embed(color=discord.Color.green(), description=("Successfully set autorole to role `%s`" % role.name)))
+            except Exception:
+                yield from error("Something went wrong while saving autorole!", message.channel, client)
+                raise Exception
--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-import discord
-import asyncio
+import asyncio as asyncio
 
-from discord import Game
+import discord
+from discord import Game, Embed
 
 import SECRETS
 import STATICS
-from commands import ping, cmd_autorole
+from commands import cmd_ping, cmd_autorole
 
 client = discord.Client()
 
 
 commands = {
 
-    "ping": ping,
+    "ping": cmd_ping,
     "autorole": cmd_autorole,
 
 }
@@ -21,33 +21,35 @@ commands = {
 @client.event
 @asyncio.coroutine
 def on_ready():
-
-    print("\nRunning on %s servers:" % len(client.servers))
-    # for s in client.servers:
-    #     print("  - %s (%s)" % (s.name, s.id))
+    print("Bot is logged in successfully. Running on servers:\n")
     [(lambda s: print("  - %s (%s)" % (s.name, s.id)))(s) for s in client.servers]
-    print("------------------------")
-
-    yield from client.change_presence(game=Game(name="THIS BOT IS RUNNING FOR TUTORIAL RECORDING"))
+    yield from client.change_presence(game=Game(name="This is just for tutorial purposes!"))
 
 
 @client.event
 @asyncio.coroutine
 def on_message(message):
-    if message.content.startswith("."):
-        invoke = message.content[1:].split(" ")[0]
+    if message.content.startswith(STATICS.PREFIX):
+        invoke = message.content[len(STATICS.PREFIX):].split(" ")[0]
         args = message.content.split(" ")[1:]
         if commands.__contains__(invoke):
-            yield from commands.get(invoke).ex(message, args, client)
+            yield from commands.get(invoke).ex(args, message, client, invoke)
         else:
-            yield from client.send_message(message.channel, embed=discord.Embed(color=discord.Color.red(), description=("Command \"%s%s\" does not exist!" % (STATICS.PREFIX, invoke))))
+            yield from client.send_message(message.channel, embed=Embed(color=discord.Color.red(), description=("The command `%s` is not valid!" % invoke)))
 
 
 @client.event
 @asyncio.coroutine
 def on_member_join(member):
-    yield from client.send_message(member, "**Hey %s!**\n\nWelcome on the  *%s*Discord Server!" % (member.name, member.server.name))
-    yield from client.add_roles(member, cmd_autorole.get(member.server))
+    yield from client.send_message(member, "**Hey %s!**\n\nWelcome on the official nice super cool *%s* discord server from %s!\n\nIf you want, you can write a little bit about you in the %s channel!\n\nNow have a nice day!" % (member.name, member.server.name, discord.utils.get(member.server.channels, id="332163979365580801").mention, member.server.owner.mention))
+    role = cmd_autorole.get(member.server)
+    if not role == None:
+        yield from client.add_roles(member, role)
+        try:
+            yield from client.send_message(member, "\n\nYou got automatically assigned the role **" + role.name + "**!")
+        except Exception:
+            yield from client.send_message(member, "Sorry, but the bot has no permissions to automatically assing you the role **" + role.name + "**.")
+            raise Exception
 
 
 client.run(SECRETS.TOKEN)
